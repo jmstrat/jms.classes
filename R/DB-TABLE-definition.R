2fcf9e6b2cbdf10797cdbbccd2ca01f5ca7662df
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2017-11-10 21:22:15 +0000

    Add validator function

--- R/DB-TABLE-definition.R
+++ R/DB-TABLE-definition.R
@@ -5,9 +5,9 @@
 #' @param version Can be used to track changes to table schema
 #' @return A database table object
 #' @export
-jms.database.table <- function(...,version=1) {
+jms.database.table <- function(...,validator=NULL,version=1) {
   log.info('Creating database table')
-  as.jms.database.table(data.frame(...),version=1)
+  as.jms.database.table(data.frame(...),validator,version=1)
 }
 
 #' Check if an object is a jms.database.table
@@ -24,19 +24,20 @@ is.jms.database.table <- function(x) {
 #' @param x The object to be converted
 #' @return The converted object
 #' @export
-as.jms.database.table <- function(x) UseMethod("as.jms.database.table")
+as.jms.database.table <- function(x,validator,version) UseMethod("as.jms.database.table")
 
 #' @export
-as.jms.database.table.default <- function(x) {
+as.jms.database.table.default <- function(x,validator,version) {
   stop("Unable to convert this class")
 }
 
 #' @export
-as.jms.database.table.data.frame <- function(x,version=1) {
+as.jms.database.table.data.frame <- function(x,validator=NULL,version=1) {
   attr(x,'.path')<-''
   attr(x,'.modTime')<-.POSIXct(0)
   attr(x,'.hasChanged')<-TRUE
   attr(x, "class") <- c("jms.database.table", "data.frame")
+  attr(x,'.validator')<-validator
   attr(x,'version')<-version
   return(x)
 }
