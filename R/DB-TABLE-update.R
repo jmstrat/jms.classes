679c46b6457162f74cef474d9dd7b67244f1c3c4
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2017-11-11 01:01:15 +0000

    Allow setting more than one row at once

--- R/DB-TABLE-update.R
+++ R/DB-TABLE-update.R
@@ -2,7 +2,8 @@
 `[<-.jms.database.table` <- function(x,i,j,value) {
   if(missing(i)) i=T
   if(missing(j)) j=T
-  log.info('Updating [%s,%s] with {%s}',i,j,paste0(value,collapse=','))
+  tt<<-value
+  log.info('Updating {[%s],[%s]} with {%s}',paste0(i,collapse=','),paste0(j,collapse=','),paste0(value,collapse=','))
   #Validate the new value
   validator=attr(x,'.validator')
   if(!is.null(validator)) {
@@ -11,12 +12,17 @@
     names(n)<-n
     log.debug('Columns: [%s]',paste0(n,collapse=','))
     tovalidate=n[j]
-    log.debug('Values supplied: [%s]',paste0(tovalidate,collapse=','))
+    log.debug('Columns supplied: [%s]',paste0(tovalidate,collapse=','))
     if(any(is.na(tovalidate))) stop('Invalid options supplied')
-    validate=value
-    names(validate)<-tovalidate
-    value<-do.call(validator,as.list(validate))[j]
-    log.info('Validated update [%s,%s] with {%s}',i,j,paste0(value,collapse=','))
+    validate=as.matrix(value)
+    #Now we need to split a (potentially) 2D matrix into 1D vectors
+    validatelist=split(validate, rep(1:ncol(validate), each = nrow(validate)))
+    names(validatelist)<-tovalidate
+    value<-do.call(validator,as.list(validatelist))[j]
+    log.debug('Values returned: [%s]',paste0(value,collapse=','))
+    #Now we need to restore the original dimensions
+    value<-matrix(unlist(value),nrow=nrow(validate),ncol=ncol(validate))
+    log.info('Validated update {[%s],[%s]} with {%s}',paste0(i,collapse=','),paste0(j,collapse=','),paste0(value,collapse=','))
   }
   #Make the change
   x<-NextMethod()
