e60d9b96cf61c62f87a9de830c1bda37ded02734
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2017-11-11 11:37:20 +0000

    Only lock if necessary

--- R/DB-save_load.R
+++ R/DB-save_load.R
@@ -1,18 +1,18 @@
 #' @export
 save.jms.database <- function(x,...) {
   log.info('Preparing to save database')
-  lock(x)
   if(x$.hasChanged) {
     path=paste0(x$.path,'/database')
+    lock(x)
     log.info('Saving database to %s',path)
     saveRDS(x$.table_names,path)
     x$.modTime=file.info(path)$mtime
+    unlock(x)
     x$.hasChanged=FALSE
   } else {
     log.info('Database does not need saving')
   }
   #Tables are saved when they are added to the database
-  unlock(x)
   return(x)
 }
 
@@ -25,20 +25,20 @@ load.jms.database <- function(x,...) {
   }
   lock(x)
   new_table_names<-readRDS(paste0(x$.path,'/database'))
-  log.info('Found tables: %s',new_table_names)
+  x$.modTime<-file.info(x$.path)$mtime
+  log.info('Found tables: %s',paste0(new_table_names,collapse=','))
   for(name in new_table_names){
     if(name %in% x$.table_names && !is.stale(x[[name,.internal=TRUE]])) {
       log.info('Table %s already loaded',name)
       next() #We already have this table!
     }
-    log.info('Loading table: %s',name)
+    log.info('Creating table: %s',name)
     #We need to load this table
-    delayedAssign('table', {
-      table=jms.database.table()
-      attr(table,'.path')<-paste0(x$.path,'/',name,'.table')
-      table<-load(table)
-      table})
-    x[[name]]<-table
+    skeleton=jms.database.table()
+    attr(skeleton,'.path')<-paste0(x$.path,'/',name,'.table')
+    attr(skeleton,'.lockfile')<-paste0(x$.path,'/',name,'.table.lock')
+    x[[name,.internal=TRUE]]<-skeleton
+    #Tables are loaded when they are got from the database
   }
   x$.hasChanged=FALSE
   unlock(x)
