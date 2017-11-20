00fd040a25d9a0285e3ea6d32ed681e9f8a458ec
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2017-11-20 13:38:30 +0000

    View method

---
+++ R/METHODS-View.R
@@ -0,0 +1,8 @@
+## make an S3 generic for View
+#' @export
+View <- function(...) UseMethod('View')
+## take the usual definition of View,
+## and set it to be the default method
+## this mget must be at run time to capture the RStudio override (i.e. wrapped in function)
+#' @export
+View.default <- function(...) mget('View', as.environment('package:utils'))[[1]](...)
