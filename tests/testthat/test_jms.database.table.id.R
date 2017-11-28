726c73a34919253705b935c0f7655d2cd02e9804
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2017-11-28 15:00:56 +0000

    More tests

--- tests/testthat/test_jms.database.table.id.R
+++ tests/testthat/test_jms.database.table.id.R
@@ -43,6 +43,7 @@ test_that("Adding a row", {
   table[]<-c(9,'new')
   expect_equal(nrow(table),4)
   expect_equal(ncol(table),3)
+  expect_is(table,'jms.database.table.id')
   df<-as.data.frame(table)
   expect_equal(df,data.frame(id=c(1,2,3,4),testcolumn=c(1,2,3,9),testcolumn2=c('test','test','test','new'),stringsAsFactors = FALSE))
 })
@@ -52,8 +53,17 @@ test_that("Updating a row", {
   table[2]<-c(99,'updated')
   expect_equal(nrow(table),3)
   expect_equal(ncol(table),3)
+  expect_is(table,'jms.database.table.id')
   df<-as.data.frame(table)
   expect_equal(df,data.frame(id=c(1,2,3),testcolumn=c(1,99,3),testcolumn2=c('test1','updated','test3'),stringsAsFactors = FALSE))
+})
+
+test_that("Getting a value", {
+  table<-jms.database.table.id(testcolumn=c(1,2,3),testcolumn2=c('test1','test2','test3'))
+  expect_equal(table[1,1],1)
+  expect_equal(table[2,'testcolumn2'],'test2')
+  expect_equivalent(table[1,],data.frame(1,1,'test1',stringsAsFactors = FALSE))
+  expect_is(table,'jms.database.table.id')
 })
 
 test_that("Removing a row", {
@@ -61,6 +71,7 @@ test_that("Removing a row", {
   table[-2]
   expect_equal(nrow(table),2)
   expect_equal(ncol(table),3)
+  expect_is(table,'jms.database.table.id')
   df<-as.data.frame(table)
   expect_equal(df,data.frame(id=c(1,3),testcolumn=c(1,3),testcolumn2=c('test1','test3'),stringsAsFactors = FALSE))
 })
@@ -71,6 +82,7 @@ test_that("Removing then adding a row", {
   table[]<-c(9,'new')
   expect_equal(nrow(table),3)
   expect_equal(ncol(table),3)
+  expect_is(table,'jms.database.table.id')
   df<-as.data.frame(table)
   expect_equal(df,data.frame(id=c(1,3,4),testcolumn=c(1,3,9),testcolumn2=c('test1','test3','new'),stringsAsFactors = FALSE))
 })
