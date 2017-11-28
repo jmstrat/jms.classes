c3f0f2cca29ff3e1d32009dd9abc033b6bc78034
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2017-11-28 13:41:55 +0000

    Add dim

--- R/DB-TABLE-dimnames.R
+++ R/DB-TABLE-dimnames.R
@@ -3,3 +3,6 @@ dimnames.jms.database.table <- function(x) dimnames(x$.table)
 
 #' @export
 `dimnames<-.jms.database.table` <- function(x,value) `dimnames<-`(x$.table,value)
+
+#' @export
+dim.jms.database.table <- function(x) dim(x$.table)
