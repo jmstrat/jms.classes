5ecc20261494ee1e45d582c468989af62a85fdc2
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2017-11-29 15:07:05 +0000

    Tests

--- tests/testthat/test_jms.database.table.R
+++ tests/testthat/test_jms.database.table.R
@@ -33,58 +33,75 @@ test_that("Conversion from data.frame", {
 
 test_that("Adding a row", {
   table<-jms.database.table(testcolumn=c(1,2,3),testcolumn2=c('test','test','test'))
-  table[4,]<-list(9,'new')
+  expect_error(table[4,]<-list(9,'new'),NA)
   expect_equal(nrow(table),4)
   expect_equal(ncol(table),2)
   expect_is(table,'jms.database.table')
-  df<-as.data.frame(table)
+  expect_error(df<-as.data.frame(table),NA)
   expect_equal(df,data.frame(testcolumn=c(1,2,3,9),testcolumn2=c('test','test','test','new'),stringsAsFactors = FALSE))
 })
-#
-# test_that("Updating a row", {
-#   table<-jms.database.table.id(testcolumn=c(1,2,3),testcolumn2=c('test1','test2','test3'))
-#   table[2]<-c(99,'updated')
-#   expect_equal(nrow(table),3)
-#   expect_equal(ncol(table),3)
-#   expect_is(table,'jms.database.table.id')
-#   df<-as.data.frame(table)
-#   expect_equal(df,data.frame(id=c(1,2,3),testcolumn=c(1,99,3),testcolumn2=c('test1','updated','test3'),stringsAsFactors = FALSE))
-# })
-#
-# test_that("Getting a value", {
-#   table<-jms.database.table.id(testcolumn=c(1,2,3),testcolumn2=c('test1','test2','test3'))
-#   expect_equal(table[1,1],1)
-#   expect_equal(table[2,'testcolumn2'],'test2')
-#   expect_equivalent(table[1,],data.frame(1,1,'test1',stringsAsFactors = FALSE))
-#   expect_is(table,'jms.database.table.id')
-# })
-#
-# test_that("Removing a row", {
-#   table<-jms.database.table.id(testcolumn=c(1,2,3),testcolumn2=c('test1','test2','test3'))
-#   table[-2]
-#   expect_equal(nrow(table),2)
-#   expect_equal(ncol(table),3)
-#   expect_is(table,'jms.database.table.id')
-#   df<-as.data.frame(table)
-#   expect_equal(df,data.frame(id=c(1,3),testcolumn=c(1,3),testcolumn2=c('test1','test3'),stringsAsFactors = FALSE))
-# })
-#
-# test_that("Removing then adding a row", {
-#   table<-jms.database.table.id(testcolumn=c(1,2,3),testcolumn2=c('test1','test2','test3'))
-#   table[-2]
-#   table[]<-c(9,'new')
-#   expect_equal(nrow(table),3)
-#   expect_equal(ncol(table),3)
-#   expect_is(table,'jms.database.table.id')
-#   df<-as.data.frame(table)
-#   expect_equal(df,data.frame(id=c(1,3,4),testcolumn=c(1,3,9),testcolumn2=c('test1','test3','new'),stringsAsFactors = FALSE))
-# })
-#
-# test_that("Invalid rows", {
-#   table<-jms.database.table.id(testcolumn=c(1,2,3),testcolumn2=c('test1','test2','test3'))
-#   expect_true(all(is.na(table[4])))
-#   expect_error(table[-4])
-#   expect_error(table[-1]<-'anything')
-# })
-# test_that("Validating", {
-# })
+
+test_that("Updating a row", {
+  table<-jms.database.table(testcolumn=c(1,2,3),testcolumn2=c('test1','test2','test3'))
+  expect_error(table[2]<-list(99,'updated'),NA)
+  expect_equal(nrow(table),3)
+  expect_equal(ncol(table),2)
+  expect_is(table,'jms.database.table')
+  expect_error(df<-as.data.frame(table),NA)
+  expect_equal(df,data.frame(testcolumn=c(1,99,3),testcolumn2=c('test1','updated','test3'),stringsAsFactors = FALSE))
+})
+
+test_that("Getting a value", {
+  table<-jms.database.table(testcolumn=c(1,2,3),testcolumn2=c('test1','test2','test3'))
+  expect_equal(table[1,1],1)
+  expect_equal(table[2,'testcolumn2'],'test2')
+  expect_equivalent(table[1,],data.frame(1,'test1',stringsAsFactors = FALSE))
+  expect_is(table,'jms.database.table')
+})
+
+test_that("Removing a row", {
+  table<-jms.database.table(testcolumn=c(1,2,3),testcolumn2=c('test1','test2','test3'))
+  expect_error(table[-2],NA)
+  expect_equal(nrow(table),2)
+  expect_equal(ncol(table),2)
+  expect_is(table,'jms.database.table')
+  expect_error(df<-as.data.frame(table),NA)
+  expect_equal(df,data.frame(testcolumn=c(1,3),testcolumn2=c('test1','test3'),stringsAsFactors = FALSE))
+})
+
+test_that("Removing then adding a row", {
+  table<-jms.database.table(testcolumn=c(1,2,3),testcolumn2=c('test1','test2','test3'))
+  expect_error(table[-2],NA)
+  expect_error(table[3]<-list(9,'new'),NA)
+  expect_equal(nrow(table),3)
+  expect_equal(ncol(table),2)
+  expect_is(table,'jms.database.table')
+  expect_error(df<-as.data.frame(table),NA)
+  expect_equal(df,data.frame(testcolumn=c(1,3,9),testcolumn2=c('test1','test3','new'),stringsAsFactors = FALSE))
+})
+
+test_that("Invalid rows", {
+  table<-jms.database.table(testcolumn=c(1,2,3),testcolumn2=c('test1','test2','test3'))
+  expect_true(all(is.na(table[4])))
+  expect_equal(table[-4],as.data.frame(table))
+  expect_error(table[-1]<-'anything')
+})
+
+test_that("Validating", {
+  validate <- function(testcolumn=numeric(),testcolumn2=character()) {
+    testcolumn=assert_numeric(testcolumn,"Invalid testcolumn")
+    testcolumn2=assert_character(testcolumn2,"Invalid testcolumn2")
+    list(testcolumn=testcolumn,testcolumn2=testcolumn2)
+  }
+  table<-jms.database.table(testcolumn=c(1,2,3),
+                            testcolumn2=c('test1','test2','test3'),
+                            validator=validate)
+  expect_is(table,'jms.database.table')
+  expect_error(table[4,]<-list(9,'new1'),NA)
+  expect_error(table[4,]<-list('notanumber','fail'))
+  expect_error(table[5,]<-list(testcolumn=2,testcolumn2='new2'),NA)
+  expect_error(table[6,]<-list(testcolumn2='new3',testcolumn=10),NA)
+  expect_is(table,'jms.database.table')
+  df<-as.data.frame(table)
+  expect_equal(df,data.frame(testcolumn=c(1,2,3,9,2,10),testcolumn2=c('test1','test2','test3','new1','new2','new3'),stringsAsFactors = FALSE))
+})
