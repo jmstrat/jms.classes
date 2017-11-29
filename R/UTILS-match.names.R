4de690324732a41db63369166fbf3309b2fffbf0
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2017-11-29 15:07:24 +0000

    Initial commit for match.names

---
+++ R/UTILS-match.names.R
@@ -0,0 +1,13 @@
+#' match the items in a list to a vector of names
+#'
+#' match.names returns a list with items ordered and names as defined by another vector
+#'
+#' @param names vector of names to be expected from list
+#' @param list list of values to be matched with names
+#' @return list with items ordered and names as defined by another vector
+#' @export
+match.names <- function(names,list) {
+  return(list)
+}
+
+
---
+++ tests/testthat/test_match.names.R
@@ -0,0 +1,42 @@
+context("match.names")
+
+expected_names=c('test1','test2','test3','test4')
+test_that("match.names works with unnamed list", {
+  valueList=list(1,2,3,4)
+  result=match.names(expected_names,valueList)
+  expect_equal(result,list(test1=1,test2=2,test3=3,test4=4))
+})
+test_that("match.names works with named list", {
+  valueList=list(test1=1,test2=2,test3=3,test4=4)
+  result=match.names(expected_names,valueList)
+  expect_equal(result,valueList)
+
+  valueList=list(test2=2,test4=4,test1=1,test3=3)
+  result=match.names(expected_names,valueList)
+  expect_equal(result,valueList)
+})
+
+test_that("match.names works with mixed named list", {
+  valueList=list(test1=1,test2=2,3,4)
+  result=match.names(expected_names,valueList)
+  expect_equal(result,list(test2=2,test4=4,test1=1,test3=3))
+
+  valueList=list(test2=2,test4=4,1,3)
+  expect_warning(result=match.names(expected_names,valueList))
+  expect_equal(result,list(test2=2,test4=4,test1=1,test3=3))
+})
+
+test_that("match.names works with incorrectly named list", {
+  valueList=list(test7=7,test8=8,test9=9,test10=10)
+  expect_error(match.names(expected_names,valueList))
+})
+
+test_that("match.names works with different length lists", {
+  valueList=list(test1=1,test2=2)
+  expect_warning(result=match.names(expected_names,valueList))
+  expect_equal(result,valueList)
+
+  valueList=list(test1=1,test2=2,test3=3,test4=4,test5=5)
+  expect_warning(result=match.names(expected_names,valueList))
+  expect_equal(result,list(test1=1,test2=2,test3=3,test4=4))
+})
