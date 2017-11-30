9f461d5f3e678f5e60d37406300e658bb3a32dea
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2017-11-30 12:54:30 +0000

    Testing templates

---
+++ tests/testthat/test_lockfile.R
@@ -0,0 +1,6 @@
+context("lockfile")
+
+test_that("TEST", {
+  #make_lockfile <- function(path,timeout=10, unique=FALSE)
+  #remove_lockfile <- function(path)
+})
---
+++ tests/testthat/test_typeValidation.R
@@ -0,0 +1,44 @@
+context("Type Validation")
+
+test_that("numeric", {
+  #verify_numeric
+  #assert_numeric
+})
+
+test_that("positive", {
+  #verify_positive
+  #assert_positive
+})
+
+test_that("character", {
+  #assert_character
+})
+
+test_that("list", {
+  #assert_list
+})
+
+test_that("date", {
+  #is.Date
+  #assert_date
+})
+
+test_that("file", {
+  #assert_file
+})
+
+test_that("directory", {
+  #assert_directory
+})
+
+test_that("logical", {
+  #assert_logical
+})
+
+test_that("colour", {
+  #assert_colour
+})
+
+test_that("function", {
+  #assert_function
+})
