4de0fa82668fcf706e3cdc451800963364763fbd
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2017-11-30 11:22:57 +0000

    Reload if checking equality

---
+++ R/DB-TABLE-equal.R
@@ -0,0 +1,13 @@
+#' @export
+all.equal.jms.database.table <- function(target,current,...) {
+  load(target)
+  load(current)
+  NextMethod()
+}
+
+#' @export
+`==.jms.database.table` <- function(e1,e2) {
+  load(e1)
+  load(e2)
+  NextMethod()
+}
