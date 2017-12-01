53d170eed82b7d5c78a5e3bae3031b1af61438f6
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2017-12-01 15:39:19 +0000

    Try to disable praise...

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,6 @@
 library(testthat)
 library(jms.classes)
 
+ProgressReporter$public_fields$show_praise=FALSE
+formals(ProgressReporter$public_methods$initialize)$show_praise=FALSE
 test_check("jms.classes")
--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -3,5 +3,3 @@ if(!exists('run_all_tests')) run_all_tests=FALSE
 skip_long <- function() {
   if (!run_all_tests) skip("Long task. Set run_all_tests=TRUE to run.")
 }
-
-ProgressReporter$public_fields$show_praise=FALSE
