f14d8adde30ec9dea70f110665c4607194381320
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2018-06-19 01:04:26 +0100

    Add undersample

---
+++ R/DO-undersample.R
@@ -0,0 +1,9 @@
+#' Undersamples a large data object to speed up processing / plotting etc.
+#'
+#' @param data The data object
+#' @param n For every n points, only the first will be taken
+#' @return The undersampled data object
+#' @export
+undersample <- function(data, n) {
+  data[c(T, rep_len(F,n-1)),]
+}
