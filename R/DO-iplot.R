ac2643a2ef0769b58af6e80d783bac4a826f4c57
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2018-06-19 01:02:30 +0100

    Add workaround for unsorted data

--- R/DO-iplot.R
+++ R/DO-iplot.R
@@ -7,7 +7,7 @@ iplotArgBlacklist<-c('labels','group') #the plot() command will ignore these
 #' The x-axis zoom level of plots within a group is automatically synchronized.
 #' @rdname iPlot
 #' @export
-iplot.jms.data.object <- function(...,offset=1/sqrt(length(ycol(data))-1),xlim=NULL,ylim=NULL,y2lim=NULL,axes=c(1,2),xlab=xlab_(data),ylab=ylab_(data),y2lab=y2lab_(data),col=par('col'),lwd=1,pch=NA,labels=NULL,group=NULL) {
+iplot.jms.data.object <- function(...,offset=1/sqrt(length(ycol(data))-1),xlim=NULL,ylim=NULL,y2lim=NULL,axes=c(1,2),xlab=xlab_(data),ylab=ylab_(data),y2lab=y2lab_(data),col=graphics::par('col'),lwd=1,pch=NA,labels=NULL,group=NULL, unsortedRangeWorkaround=T) {
   data<-combine(unname(list(...)),interpolate=TRUE) #Need to interpolate to avoid gaps...
   dots <- substitute(list(...))[-1]
   if(length(labels)) {
@@ -27,8 +27,22 @@ iplot.jms.data.object <- function(...,offset=1/sqrt(length(ycol(data))-1),xlim=N
   if(any(is.null(y2lim)) && !all(is.na(y2col(data)))) y2lim=range(data[,y2col(data)],na.rm = T)
 
   xrange=range(data[,1],na.rm=T)
-  if(xlim[[2]]>xrange[[2]]) xlim[[2]]<-xrange[[2]]
-  if(xlim[[1]]<xrange[[1]]) xlim[[1]]<-xrange[[1]]
+  i=1; j=2
+  if(xlim[[1]]>xlim[[2]]) {j=1; i=2}
+  if(xlim[[j]]>xrange[[2]]) xlim[[j]]<-xrange[[2]]
+  if(xlim[[i]]<xrange[[1]]) xlim[[i]]<-xrange[[1]]
+
+  if(unsortedRangeWorkaround) {
+    # By default dygraphs expects data to be sorted in x
+    # Thus, when it "resets" the xrange it simply set it to cover
+    # the range between the first and last values, this is not
+    # necessarily true of unsorted data, so we add two invisible
+    # points at the start and the end to trick it.
+    data = rbind(NA, data, NA)
+    data[1,1]<-xlim[[1]]
+    data[nrow(data),1]<-xlim[[2]]
+  }
+
   graph<-dygraphs::dygraph(data,group=group)
   col_all=if(is.null(col)) NULL else expand_args(2:(ncol(data)),col)[[2]]
   lwd_all=if(is.null(lwd)) NULL else expand_args(2:(ncol(data)),lwd)[[2]]
@@ -39,7 +53,7 @@ iplot.jms.data.object <- function(...,offset=1/sqrt(length(ycol(data))-1),xlim=N
     drawPoints <- if(!is.na(pch_all[[i]])) TRUE else NULL
     pointSize <- if(!is.na(pch_all[[i]])) 1 else 0
     strokeWidth <- lwd_all[[i]]
-    label<- if(!is.null(labels)) labels[[i]] else NULL
+    label<- if(!is.null(labels)&&length(labels)>=i) labels[[i]] else NULL
     axis<- if(i>y1end) 'y2' else 'y'
     graph<-dygraphs::dySeries(graph,label=label,color=grDevices::rgb(t(grDevices::col2rgb(col)/255)),axis=axis,drawPoints=drawPoints,pointSize=pointSize,strokeWidth=strokeWidth)
   }
