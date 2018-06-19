73fbe61e7111b183b79154def0f38bc4b20c2067
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2018-06-19 01:06:55 +0100

    Improve findMethod

--- R/UTILS-find-method.R
+++ R/UTILS-find-method.R
@@ -1,6 +1,7 @@
+# https://stackoverflow.com/questions/42738851
 findMethod <- function(generic, ...) {
   ch <- deparse(substitute(generic))
   f <- X <- function(x, ...) UseMethod("X")
-  for(m in methods(ch)) assign(sub(ch, "X", m), "body<-"(f, value = m))
+  for(m in utils::methods(ch)) assign(sub(ch, "X", m, fixed = TRUE), "body<-"(f, value = m))
   X(...)
 }
