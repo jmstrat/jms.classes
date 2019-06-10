9f9ba3e282cefcb8d16854e1aa4550bbeed12212
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-10 15:05:51 +0100

    Allow exporting a list of data objects

--- R/METHODS-export.R
+++ R/METHODS-export.R
@@ -9,3 +9,21 @@ export <- function(x,path) UseMethod("export")
 export.default <- function(x,path) {
   stop("Unable to export data for this class")
 }
+#' @export
+export.list <- function(x, path) {
+  # Do we have an export method for every item in the list?
+  if(any(as.character(.S3methods(export)) %in% paste0('export.',Reduce(intersect, lapply(x, class))))) {
+    n <- names(x)
+    fn <- tools::file_path_sans_ext(path)
+    ext <- tools::file_ext(path)
+
+    paths <- paste0(fn, '_', n, '.', ext)
+
+    mapply(function(y, p) export(y, p), x, paths)
+  } else {
+    export.default(x, path)
+  }
+  return(invisible())
+}
+
+
