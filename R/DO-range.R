bb82c4eb8664657b25237c2c901b58a06d7b15d1
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-10 15:10:49 +0100

    Add min / max methods for data objects, improve range

--- R/DO-range.R
+++ R/DO-range.R
@@ -1,18 +1,67 @@
 #' Calculate Range
 #'
-#' This function calculates the range of a data objects
+#' This function calculates the range of a data object
 #' @param x The data object
 #' @return
 #' The range
 #' @examples
 #' range(data)
 #' @export
-range.jms.data.object <- function(x,offset=0,...) {
+range.jms.data.object <- function(x,offset,...) {
   if(!is.jms.data.object(x)) stop("x must be a jms.data.object")
-  yc=ycol(x)
-  if(offset>0) {
-    ym=max(x[,ycol(x)])
-    for(i in 1:length(yc)) x[,yc[[i]]]=x[,yc[[i]]]+offset*(i-1)*ym
+  yc = tryCatch(ycol(x), error = function(e) NULL)
+  x = as.matrix(x)
+  if(!missing(offset)) {
+    ym=max(x[,yc])
+    ny = length(yc)
+    nx = nrow(x)
+    offsets = matrix(rep.int(offset*(0:(ny-1))*ym, nx), nx, ny, T)
+    x[,yc]<-.Primitive("+")(x[,yc],offsets)
   }
-  range.default(x[,ycol(x)],na.rm=TRUE)
+  if(length(yc)>0) {
+    range.default(x[,yc],na.rm=TRUE)
+  } else {
+    range.default(x,na.rm=TRUE)
+  }
+}
+
+
+#' Calculate Min
+#'
+#' This function calculates the minimum y value of a data object
+#' @param x The data object
+#' @return
+#' The minimum y value
+#' @examples
+#' min(data)
+#' @export
+min.jms.data.object <- function(x, ...) {
+  if(!is.jms.data.object(x)) stop("x must be a jms.data.object")
+  yc = tryCatch(ycol(x), error = function(e) NULL)
+  x = as.matrix(x)
+  if(length(yc)>0) {
+    .Primitive("min")(x[,yc],na.rm=TRUE)
+  } else {
+    .Primitive("min")(x,na.rm=TRUE)
+  }
+}
+
+#' Calculate Max
+#'
+#' This function calculates the minimum y value of a data object
+#' @param x The data object
+#' @return
+#' The maximum y value
+#' @examples
+#' max(data)
+#' @export
+max.jms.data.object <- function(x, ...) {
+  if(!is.jms.data.object(x)) stop("x must be a jms.data.object")
+  yc = tryCatch(ycol(x), error = function(e) NULL)
+  x = as.matrix(x)
+  if(length(yc)>0) {
+    .Primitive("max")(x[,yc],na.rm=TRUE)
+  } else {
+    .Primitive("max")(x,na.rm=TRUE)
+  }
 }
