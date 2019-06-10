3d2992b944414fb2b305f8034ab64605f3c4ffca
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-10 15:12:05 +0100

    Improve export function

--- R/DO-export.R
+++ R/DO-export.R
@@ -1,16 +1,32 @@
 #' @export
 export.jms.data.object <- function(x,path) {
+  log.info('Exporting data object to "%s"', path)
   atts=attributes(x)
   attNames=names(atts)
   attNames=attNames[!attNames %in% c("names","row.names","class",'y_type','x_type','x_column','y_column', 'y2_type','y2_column')]
   f <- file(path, "w")
+
+  if(length(attNames)) {
+    writeLines('==== Data Attributes ====', f)
+  }
+
   for(att in attNames) {
     if(inherits(atts[[att]],'data.frame')) {
       writeLines(paste0(att,':'),f)
       utils::write.table(atts[[att]],f,sep=",",row.names =FALSE)
+    } else if(inherits(atts[[att]],'list')) {
+      writeLines(paste0(att,':'), f)
+      ul <- unlist(atts[[att]], T, T)
+      writeLines(paste('', names(ul), ul, sep=','), f)
     } else
       writeLines(paste(att, atts[[att]],sep=','),f)
   }
+
+  if(length(attNames)) {
+    writeLines('',f)
+    writeLines('==== Data ====', f)
+  }
+
   utils::write.table(x,f,sep=",",row.names =FALSE)
   close(f)
 }
