0f550520e0979135bf3a9bc71fe3cd671fadd96f
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-14 19:20:26 +0100

    lint

--- R/DO-extend-class.R
+++ R/DO-extend-class.R
@@ -22,62 +22,62 @@
 #' @param inherits Name of any additional data types from which this should inherit
 #' @param envir The environment within which to define the new functions
 #' @export
-create_data_type <- function(name,xlab,ylab,inherits=c(),envir=parent.frame(),y2lab=NA) {
-  log.debug('Creating new data object type %s', name)
-  dataObjName=paste0(name,'.data.object')
-  inheritsNames=if(length(inherits)) paste0(inherits,'.data.object') else NULL
+create_data_type <- function(name, xlab, ylab, inherits=c(), envir=parent.frame(), y2lab=NA) {
+  log.debug("Creating new data object type %s", name)
+  dataObjName <- paste0(name, ".data.object")
+  inheritsNames <- if (length(inherits)) paste0(inherits, ".data.object") else NULL
   asDataObjFun <- function(x) {
-    x=as.jms.data.object(x)
-    class(x) <- c(dataObjName,inheritsNames,class(x))
-    ylab(x)<-ylab
-    y2lab(x)<-y2lab
-    xlab(x)<-xlab
+    x <- as.jms.data.object(x)
+    class(x) <- c(dataObjName, inheritsNames, class(x))
+    ylab(x) <- ylab
+    y2lab(x) <- y2lab
+    xlab(x) <- xlab
     x
   }
-  asName=paste0('as.',dataObjName)
-  if(exists(asName,envir=envir)) asName=paste0(asName,'.super')
-  assign(asName,asDataObjFun,envir=envir)
+  asName <- paste0("as.", dataObjName)
+  if (exists(asName, envir=envir)) asName <- paste0(asName, ".super")
+  assign(asName, asDataObjFun, envir=envir)
 
   isDataObjFun <- function(x) {
-    return(inherits(x,dataObjName))
+    return(inherits(x, dataObjName))
   }
-  isName=paste0('is.',dataObjName)
-  if(exists(isName,envir=envir)) isName=paste0(isName,'.super')
-  assign(isName,isDataObjFun,envir=envir)
+  isName <- paste0("is.", dataObjName)
+  if (exists(isName, envir=envir)) isName <- paste0(isName, ".super")
+  assign(isName, isDataObjFun, envir=envir)
 
   dataObjFun <- function(...) {
     return(asDataObjFun(jms.data.object(...)))
   }
-  dataObjFunName=dataObjName
-  if(exists(dataObjFunName,envir=envir)) dataObjFunName=paste0(dataObjFunName,'.super')
-  assign(dataObjFunName,dataObjFun,envir=envir)
+  dataObjFunName <- dataObjName
+  if (exists(dataObjFunName, envir=envir)) dataObjFunName <- paste0(dataObjFunName, ".super")
+  assign(dataObjFunName, dataObjFun, envir=envir)
 
   readTableFun <- function(...) {
     return(asDataObjFun(read.table.jms(...)))
   }
-  readName=paste0('read.table.',name)
-  if(exists(readName,envir=envir)) readName=paste0(readName,'.super')
-  assign(readName,readTableFun,envir=envir)
+  readName <- paste0("read.table.", name)
+  if (exists(readName, envir=envir)) readName <- paste0(readName, ".super")
+  assign(readName, readTableFun, envir=envir)
 
-  subsetFun <- function(x,...) {
-    #Get the function call, and extract the class name from it (TODO: is there a better way???)
-    me=sub('^`\\[\\.([^\\(`]*?)(\\.super)?`.*$','\\1',deparse(sys.call(), nlines=1))
-    #Save the current class
-    myclass=class(x)
-    i=which(myclass==me)
-    #Remove everything preceeding and including the class for which this function was called
-    if(length(i)<length(myclass)) class(x)<-myclass[i+1:length(myclass)]
-    #Rerun the subset function for the remaining classes
-    #(DO NOT USE UseMethod or NextMethod: the former doesn't return to this
-    #function, the latter won't work if the "super" version of this function was called)
-    r <- `[`(x,...)
-    #Restore the original class (unless the subset resulted in e.g. a numeric)
-    if(inherits(r,'jms.data.object')) class(r)<-myclass
+  subsetFun <- function(x, ...) {
+    # Get the function call, and extract the class name from it (TODO: is there a better way???)
+    me <- sub("^`\\[\\.([^\\(`]*?)(\\.super)?`.*$", "\\1", deparse(sys.call(), nlines=1))
+    # Save the current class
+    myclass <- class(x)
+    i <- which(myclass == me)
+    # Remove everything preceeding and including the class for which this function was called
+    if (length(i) < length(myclass)) class(x) <- myclass[i + 1:length(myclass)]
+    # Rerun the subset function for the remaining classes
+    # (DO NOT USE UseMethod or NextMethod: the former doesn't return to this
+    # function, the latter won't work if the "super" version of this function was called)
+    r <- `[`(x, ...)
+    # Restore the original class (unless the subset resulted in e.g. a numeric)
+    if (inherits(r, "jms.data.object")) class(r) <- myclass
     return(r)
   }
-  subsetName=paste0('[.',dataObjName)
-  subsetBaseName = subsetName
-  if(exists(subsetName,envir=envir)) subsetName=paste0(subsetName,'.super')
-  assign(subsetName,subsetFun,envir=envir)
+  subsetName <- paste0("[.", dataObjName)
+  subsetBaseName <- subsetName
+  if (exists(subsetName, envir=envir)) subsetName <- paste0(subsetName, ".super")
+  assign(subsetName, subsetFun, envir=envir)
   registerS3method("[", dataObjName, subsetBaseName, envir=envir)
 }
