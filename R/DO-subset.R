603129a29e517d952ea5c2e0e77c0d6626ad6951
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-14 19:30:11 +0100

    lint

--- R/DO-subset.R
+++ R/DO-subset.R
@@ -1,32 +1,32 @@
 #' Retain attributes upon subsetting
 #' @export
-`[.jms.data.object` <- function (x, ...) {
+`[.jms.data.object` <- function(x, ...) {
   r <- NextMethod("[")
   # Restore attributes:
   # Don't restore these
-  special_attrs=c('class', 'comment', 'dim', 'dimnames', 'names', 'row.names', 'tsp')
-  oldAtts=attributes(x)
-  oldAtts=oldAtts[!names(oldAtts)%in%special_attrs]
-  nOldAtts=names(oldAtts)
-  attributes(r)[nOldAtts]<-oldAtts
+  special_attrs <- c("class", "comment", "dim", "dimnames", "names", "row.names", "tsp")
+  oldAtts <- attributes(x)
+  oldAtts <- oldAtts[!names(oldAtts) %in% special_attrs]
+  nOldAtts <- names(oldAtts)
+  attributes(r)[nOldAtts] <- oldAtts
 
   new_cols <- match(colnames(r), colnames(x))
 
   # Check we still have the x column after subsetting and whether its number has changed
-  if('x_column'%in%nOldAtts) {
-      attributes(r)[['x_column']]=which(new_cols%in%oldAtts[['x_column']])
+  if ("x_column" %in% nOldAtts) {
+    attributes(r)[["x_column"]] <- which(new_cols %in% oldAtts[["x_column"]])
   }
   # Check which y columns we still have and get their new numbers
-  if('y_column'%in%nOldAtts) {
-    attributes(r)[['y_column']]=which(new_cols%in%oldAtts[['y_column']])
+  if ("y_column" %in% nOldAtts) {
+    attributes(r)[["y_column"]] <- which(new_cols %in% oldAtts[["y_column"]])
   }
   # Check which y2 columns we still have and get their new numbers
-  if('y2_column'%in%nOldAtts) {
-    attributes(r)[['y2_column']]=which(new_cols%in%oldAtts[['y2_column']])
+  if ("y2_column" %in% nOldAtts) {
+    attributes(r)[["y2_column"]] <- which(new_cols %in% oldAtts[["y2_column"]])
   }
   # Restore the class if the dataset is still 2D
-  if(inherits(r,'data.frame')) class(r) <- c("jms.data.object","data.frame")
-  return (r)
+  if (inherits(r, "data.frame")) class(r) <- c("jms.data.object", "data.frame")
+  return(r)
 }
 
 
