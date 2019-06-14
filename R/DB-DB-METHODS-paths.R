61fe64e5bb685bdadfdca33be684b3b1f0add2aa
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-14 18:57:25 +0100

    lint

--- R/DB-DB-METHODS-paths.R
+++ R/DB-DB-METHODS-paths.R
@@ -1,11 +1,11 @@
 getPathForTable <- function(database, name) {
-  dbPath<-database$.path
-  if(!length(dbPath)) stop('Path is undefined')
-  paste0(dbPath,'/',name,'.table')
+  dbPath <- database$.path
+  if (!length(dbPath)) stop("Path is undefined")
+  paste0(dbPath, "/", name, ".table")
 }
 
 getPathForDatabase <- function(database) {
-  dbPath<-database$.path
-  if(!length(dbPath)) stop('Path is undefined')
-  paste0(dbPath,'/database')
+  dbPath <- database$.path
+  if (!length(dbPath)) stop("Path is undefined")
+  paste0(dbPath, "/database")
 }
