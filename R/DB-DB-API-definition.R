116b94988b0adb1c3bcfa5f40b0ee636b28bae00
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-14 18:50:01 +0100

    lint

--- R/DB-DB-API-definition.R
+++ R/DB-DB-API-definition.R
@@ -6,10 +6,10 @@
 #' @return Database object
 #' @export
 jms.database <- function(path=NULL) {
-  log.info('Creating new database with path: %s',path)
-  self<-new.env(parent=emptyenv())
-  self$.path=path
-  self$.self=self
+  log.info("Creating new database with path: %s", path)
+  self <- new.env(parent=emptyenv())
+  self$.path <- path
+  self$.self <- self
   as.jms.database(self)
 }
 
@@ -19,7 +19,7 @@ jms.database <- function(path=NULL) {
 #' @return TRUE / FALSE
 #' @export
 is.jms.database <- function(x) {
-  return(inherits(x,"jms.database"))
+  return(inherits(x, "jms.database"))
 }
 
 #' Convert an object into a jms.database
@@ -31,72 +31,72 @@ as.jms.database <- function(x) UseMethod("as.jms.database")
 
 #' @export
 as.jms.database.default <- function(x) {
-  tryCatch(as.jms.database(as.environment(x)),error = function(e) stop("Unable to convert this class"))
+  tryCatch(as.jms.database(as.environment(x)), error=function(e) stop("Unable to convert this class"))
 }
 
 #' @export
 as.jms.database.environment <- function(x) {
-  self<-get0('.self',envir=x, ifnotfound=x)
+  self <- get0(".self", envir=x, ifnotfound=x)
   database.method <- database.method.generator(self)
   database.var <- database.var.generator(self)
 
-  ##Values:
-  database.var('.path',NULL)
-  database.var('.table_names',c())
-  database.var('.hash','')
-  database.var('.tableHashes',list())
-  database.var('.hasChanged',TRUE)
-  ##Methods:
-  #Table
-  database.method('.addTable',addTable)
-  database.method('.getTable',getTable)
-  database.method('.removeTable',removeTable)
-  #Save_Load
-  database.method('.saveTable',saveTable)
-  database.method('.loadTable',loadTable)
-  database.method('.saveDatabase',saveDatabase)
-  database.method('.loadDatabase',loadDatabase)
-  #Paths
-  database.method('.getPathForTable',getPathForTable)
-  database.method('.getPathForDatabase',getPathForDatabase)
-  #Hash
-  database.method('.getHashForTable',getHashForTable)
-  database.method('.getHashForDatabase',getHashForDatabase)
-  database.method('.checkHashForTable',checkHashForTable)
-  database.method('.checkHashForDatabase',checkHashForDatabase)
-  database.method('.updateHashForTable',updateHashForTable)
-  database.method('.updateHashForDatabase',updateHashForDatabase)
-  #Lock
-  database.method('.lockTable',lockTable)
-  database.method('.unlockTable',unlockTable)
-  database.method('.lockDatabase',lockDatabase)
-  database.method('.unlockDatabase',unlockDatabase)
-  #Plot
-  database.method('.plotDatabase',plotDatabase)
+  ## Values:
+  database.var(".path", NULL)
+  database.var(".table_names", c())
+  database.var(".hash", "")
+  database.var(".tableHashes", list())
+  database.var(".hasChanged", TRUE)
+  ## Methods:
+  # Table
+  database.method(".addTable", addTable)
+  database.method(".getTable", getTable)
+  database.method(".removeTable", removeTable)
+  # Save_Load
+  database.method(".saveTable", saveTable)
+  database.method(".loadTable", loadTable)
+  database.method(".saveDatabase", saveDatabase)
+  database.method(".loadDatabase", loadDatabase)
+  # Paths
+  database.method(".getPathForTable", getPathForTable)
+  database.method(".getPathForDatabase", getPathForDatabase)
+  # Hash
+  database.method(".getHashForTable", getHashForTable)
+  database.method(".getHashForDatabase", getHashForDatabase)
+  database.method(".checkHashForTable", checkHashForTable)
+  database.method(".checkHashForDatabase", checkHashForDatabase)
+  database.method(".updateHashForTable", updateHashForTable)
+  database.method(".updateHashForDatabase", updateHashForDatabase)
+  # Lock
+  database.method(".lockTable", lockTable)
+  database.method(".unlockTable", unlockTable)
+  database.method(".lockDatabase", lockDatabase)
+  database.method(".unlockDatabase", unlockDatabase)
+  # Plot
+  database.method(".plotDatabase", plotDatabase)
 
   attr(x, "class") <- c("jms.database", "environment")
   return(x)
 }
 
 #' @export
-save.jms.database <- function(x,...) {
+save.jms.database <- function(x, ...) {
   x$.saveDatabase()
 }
 
 #' @export
-load.jms.database <- function(x,...) {
+load.jms.database <- function(x, ...) {
   x$.loadDatabase()
 }
 
 #' @export
 lock.jms.database <- function(x) {
-  log.info('Locking database')
+  log.info("Locking database")
   x$.lockDatabase()
 }
 
 #' @export
 unlock.jms.database <- function(x) {
-  log.info('Unlocking database')
+  log.info("Unlocking database")
   x$.unlockDatabase()
 }
 
@@ -106,30 +106,29 @@ is.stale.jms.database <- function(x) {
 }
 
 #' @export
-plot.jms.database <- function(x,...) {
+plot.jms.database <- function(x, ...) {
   x$.plotDatabase()
 }
 
 #' @export
-print.jms.database <- function (x,...) {
+print.jms.database <- function(x, ...) {
   load(x)
-  cat('jms.database with the following tables:',paste0(x$.table_names,collapse=', '))
+  cat("jms.database with the following tables:", paste0(x$.table_names, collapse=", "))
 }
 
 #' @export
-View.jms.database <- function(x,...) {
-  tables<-mget(x$.table_names,envir=x)
-  View(lapply(tables,as.data.frame),...,title=paste(deparse(substitute(x))[1]))
+View.jms.database <- function(x, ...) {
+  tables <- mget(x$.table_names, envir=x)
+  View(lapply(tables, as.data.frame), ..., title=paste(deparse(substitute(x))[1]))
 }
 
 #' @export
 database2reference <- function(database) {
-  if(!is.jms.database(database)) stop('database must be a jms.database')
-  #Make a new environment whose methods will continue to point to the old
-  #database in effect this gives us a 2nd pointer to the environment but with
-  #a differnent class. Simply updating the class would change this globally
-  db<-new.env(parent=database)
-  class(db)<-c('jms.database','environment')
+  if (!is.jms.database(database)) stop("database must be a jms.database")
+  # Make a new environment whose methods will continue to point to the old
+  # database in effect this gives us a 2nd pointer to the environment but with
+  # a differnent class. Simply updating the class would change this globally
+  db <- new.env(parent=database)
+  class(db) <- c("jms.database", "environment")
   db
 }
-
