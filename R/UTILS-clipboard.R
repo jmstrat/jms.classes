694828e9ba63fe77d8c767a51b0b64087f0a306f
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-14 20:15:16 +0100

    lint

--- R/UTILS-clipboard.R
+++ R/UTILS-clipboard.R
@@ -3,12 +3,12 @@
 #' @export
 #' @rdname clipboard
 clipboard_paste <- function() {
-  if(Sys.info()["sysname"] == "Darwin") {
-    file = pipe("pbpaste")
-    data = scan(file, 'character', sep='\n', quiet=TRUE)
+  if (Sys.info()["sysname"] == "Darwin") {
+    file <- pipe("pbpaste")
+    data <- scan(file, "character", sep="\n", quiet=TRUE)
     close(file)
   } else {
-    data = readLines("clipboard", warn=FALSE)
+    data <- readLines("clipboard", warn=FALSE)
   }
   data
 }
@@ -18,8 +18,8 @@ clipboard_paste <- function() {
 #' @export
 #' @rdname clipboard
 clipboard_copy <- function(string) {
-  if(Sys.info()["sysname"] == "Darwin") {
-    file = pipe("pbcopy", "w")
+  if (Sys.info()["sysname"] == "Darwin") {
+    file <- pipe("pbcopy", "w")
     write(string, file)
     close(file)
   } else {
