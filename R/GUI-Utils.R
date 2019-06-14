35d5828471f5a7d1fc54bb1e78885bded0fd49eb
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-14 19:52:24 +0100

    lint

--- R/GUI-Utils.R
+++ R/GUI-Utils.R
@@ -6,8 +6,8 @@
 #' @param session The session object
 #' @export
 #' @rdname errors
-show_error <- function(e,errorid, session) {
-  shinysky::showshinyalert(session, session$ns(errorid), paste0("ERROR: ",e), styleclass = "danger")
+show_error <- function(e, errorid, session) {
+  shinysky::showshinyalert(session, session$ns(errorid), paste0("ERROR: ", e), styleclass="danger")
   log.error(e)
 }
 
@@ -24,47 +24,59 @@ hide_error <- function(errorid, session) {
 #' @param ... Further tags to set on the button
 #' @export
 #' @rdname buttons
-normal_button <-function(id,label,...) {
-  shiny::actionButton(id, label = label,...)
+normal_button <- function(id, label, ...) {
+  shiny::actionButton(id, label=label, ...)
 }
 
 #' Create a push button with the delete icon
 #' @export
 #' @rdname buttons
-delete_button <-function(id,...) {
-  shiny::tags$button(id = id, type = "button", class = "btn action-button btn-danger", shiny::icon('trash-o'), '',...)
+delete_button <- function(id, ...) {
+  shiny::tags$button(id=id, type="button", class="btn action-button btn-danger", shiny::icon("trash-o"), "", ...)
 }
 
 #' Create a push button with the info icon
 #' @export
 #' @rdname buttons
-help_button <- function(id,...) {
-  shiny::tags$button(id=id,type="button", class = "btn btn-default action-button",'?',style="font-weight:bold;font-family: \"Times New Roman\", Times, serif; background-color:#4169E1; color: #fff;", ...)
+help_button <- function(id, ...) {
+  shiny::tags$button(
+    id=id, type="button", class="btn btn-default action-button", "?",
+    style="font-weight:bold;font-family: \"Times New Roman\", Times, serif; background-color:#4169E1; color: #fff;", ...
+  )
 }
 
 #' Create a push button with the help icon
 #' @export
 #' @rdname buttons
-info_button <- function(id,...) {
-  shiny::tags$button(id=id,type="button", class = "btn btn-default",'i',style="font-weight:bold;font-family: \"Times New Roman\", Times, serif;", ...)
+info_button <- function(id, ...) {
+  shiny::tags$button(
+    id=id, type="button", class="btn btn-default", "i",
+    style="font-weight:bold;font-family: \"Times New Roman\", Times, serif;", ...
+  )
 }
 
 #' Create a checkbox input with support for extra tags
 #' @param id The ID for the input
 #' @param ... Further tags to set on the input
 #' @export
-checkbox <- function(id,...,checked=TRUE) {
-  if(checked)
-    shiny::tags$input(id=id,type="checkbox",checked="checked",...)
-  else
-    shiny::tags$input(id=id,type="checkbox",...)
+checkbox <- function(id, ..., checked=TRUE) {
+  if (checked) {
+    shiny::tags$input(id=id, type="checkbox", checked="checked", ...)
+  } else {
+    shiny::tags$input(id=id, type="checkbox", ...)
+  }
 }
 
 #' Make a horizontal rule
 #' @param css colour
 #' @export
-hr <- function(col,...) {
-  shiny::tags$hr(style=paste0("width: 30px; height: auto; border: 1px solid ",col,"; margin: 0; margin-top: 8px; padding:0; vertical-align: middle"))
+hr <- function(col, ...) {
+  shiny::tags$hr(
+    style=paste0(
+      "width: 30px; height: auto; border: 1px solid ", col,
+      "; margin: 0; margin-top: 8px; padding:0; vertical-align: middle"
+    )
+  )
 }
 
 #' Make a vector of shiny inputs
@@ -74,30 +86,38 @@ hr <- function(col,...) {
 #' @return The vector of buttons
 #' @keywords internal
 #' @rdname inputs
-make_inputs <- function(input_id,ids,templateFunction,...,session=NULL) {
-  input_id2 <- if(!is.null(session)) session$ns(input_id) else input_id
-  template<-templateFunction('{id}',...,counter=0,onclick = paste0('this.setAttribute(\"counter\",parseInt(this.getAttribute(\"counter\"))+1);Shiny.onInputChange(\"',input_id2,'\",  this.id+\"_\"+this.getAttribute(\"counter\"))'))
+make_inputs <- function(input_id, ids, templateFunction, ..., session=NULL) {
+  input_id2 <- if (!is.null(session)) session$ns(input_id) else input_id
+  template <- templateFunction("{id}", ...,
+    counter=0,
+    onclick=paste0(
+      'this.setAttribute(\"counter\",parseInt(this.getAttribute(\"counter\"))+1);Shiny.onInputChange(\"',
+      input_id2, '\",  this.id+\"_\"+this.getAttribute(\"counter\"))'
+    )
+  )
   ### -->
   # Slightly faster version of as.character
-  html <- htmltools:::paste8("<", template$name, sep = "")
+  html <- htmltools:::paste8("<", template$name, sep="")
   attribs <- lapply(template$attribs, as.character)
   for (attrib in names(attribs)) {
     attribValue <- attribs[[attrib]]
-    text <- htmltools::htmlEscape(attribValue, attribute = TRUE)
-    html<-htmltools:::paste8(html," ", attrib, "=\"", text, "\"", sep = "")
+    text <- htmltools::htmlEscape(attribValue, attribute=TRUE)
+    html <- htmltools:::paste8(html, " ", attrib, "=\"", text, "\"", sep="")
   }
   children <- htmltools:::dropNullsOrEmpty(htmltools:::flattenTags(template$children))
-  html<-paste0(html,'>')
-  for(child in children)
-    html<-htmltools:::paste8(html,htmltools:::normalizeText(child), "</", template$name, ">", sep = "")
+  html <- paste0(html, ">")
+  for (child in children) {
+    html <- htmltools:::paste8(html, htmltools:::normalizeText(child), "</", template$name, ">", sep="")
+  }
   ### <--
   html <- gsub("%", "%%", html)
   html <- gsub("\\{id\\}", "%1$s", html)
   # Much faster than looping the above, but make sure %s is the only placeholder!
-  if(input_id!='')
-    sprintf(html,paste0(input_id2, '_',ids))
-  else
-    sprintf(html,ids)
+  if (input_id != "") {
+    sprintf(html, paste0(input_id2, "_", ids))
+  } else {
+    sprintf(html, ids)
+  }
 }
 
 #' @param inputs see details
@@ -114,54 +134,54 @@ make_inputs <- function(input_id,ids,templateFunction,...,session=NULL) {
 #'  Buttons are generated using \code{make_inputs}.
 #' @return \code{list(data=<data with extra columns>, htmlCols=<column #s with HTML>)}
 #' @examples
-#' inputs=list(
-#' list(
-#'   name='test',
-#'   id='checkbox',
-#'   ids=rownames(data),
-#'   template=checkbox,
-#'   column=1
-#' ),
-#' list(
-#'   name='hr',
-#'   id='',
-#'   ids=rgb(t(col2rgb(plotrix::color.gradient(c(1,0),0,c(0,1),nslices=nrow(mydata)))/255)),
-#'   template=hr,
-#'   column=3
-#' )
+#' inputs <- list(
+#'   list(
+#'     name="test",
+#'     id="checkbox",
+#'     ids=rownames(data),
+#'     template=checkbox,
+#'     column=1
+#'   ),
+#'   list(
+#'     name="hr",
+#'     id="",
+#'     ids=rgb(t(col2rgb(plotrix::color.gradient(c(1, 0), 0, c(0, 1), nslices=nrow(mydata))) / 255)),
+#'     template=hr,
+#'     column=3
+#'   )
 #' )
 #' @keywords internal
 #' @rdname inputs
-addInputsToData <- function(data,inputs,session=NULL) {
-  positions=c()
-  if(!is.null(inputs)) {
-    ncol_before=ncol(data)
-    for(input in inputs) {
-      id=input$id
-      template=input$template
-      ids <- if(!is.null(input$ids)) input$ids else data[,'id']
-      varArgs=input[!names(input)%in%c('name','id','template','column','ids')]
-      inputColumn=do.call(make_inputs,list(input_id=id,templateFunction=template,ids=ids,varArgs,session=session))
-      #add the column to the end
-      data<-cbind(data,inputColumn)
-      colnames(data)[ncol(data)]<-input$name
-      positions=c(positions,input$column)
+addInputsToData <- function(data, inputs, session=NULL) {
+  positions <- c()
+  if (!is.null(inputs)) {
+    ncol_before <- ncol(data)
+    for (input in inputs) {
+      id <- input$id
+      template <- input$template
+      ids <- if (!is.null(input$ids)) input$ids else data[, "id"]
+      varArgs <- input[!names(input) %in% c("name", "id", "template", "column", "ids")]
+      inputColumn <- do.call(make_inputs, list(input_id=id, templateFunction=template, ids=ids, varArgs, session=session))
+      # add the column to the end
+      data <- cbind(data, inputColumn)
+      colnames(data)[ncol(data)] <- input$name
+      positions <- c(positions, input$column)
     }
-    #Reorder
-    data<-as.data.frame(data[,c(1:ncol_before,1:length(positions)+ncol_before)[order(c(1:ncol_before,positions-0.5))]])
+    # Reorder
+    data <- as.data.frame(data[, c(1:ncol_before, 1:length(positions) + ncol_before)[order(c(1:ncol_before, positions - 0.5))]])
   }
-  list(data=data,htmlCols=positions)
+  list(data=data, htmlCols=positions)
 }
 
 #' @keywords internal
 #' @rdname inputs
-inputReactives <- function(inputs,input) {
-  irs=c()
-  for(i in inputs) {
-    id=i$id
-    irs=c(irs,shiny::eventReactive(input[[id]],{
-      components=strsplit(input[[id]],'_')[[1]]
-      components[[length(components)-1]]
+inputReactives <- function(inputs, input) {
+  irs <- c()
+  for (i in inputs) {
+    id <- i$id
+    irs <- c(irs, shiny::eventReactive(input[[id]], {
+      components <- strsplit(input[[id]], "_")[[1]]
+      components[[length(components) - 1]]
     }))
   }
   irs
@@ -169,7 +189,9 @@ inputReactives <- function(inputs,input) {
 
 #' @rdname disableInput
 #' @export
-jsCodeHandler <- function() {shiny::tags$script(shiny::HTML('Shiny.addCustomMessageHandler("jsCode",function(message) {eval(message.code);});'))}
+jsCodeHandler <- function() {
+  shiny::tags$script(shiny::HTML('Shiny.addCustomMessageHandler("jsCode",function(message) {eval(message.code);});'))
+}
 
 #' Disable or enable an input
 #'
@@ -177,18 +199,18 @@ jsCodeHandler <- function() {shiny::tags$script(shiny::HTML('Shiny.addCustomMess
 #' @param session The session object
 #' @export
 #' @rdname disableInput
-disableInput <- function(id,session) session$sendCustomMessage(type="jsCode",list(code= paste("$('#",session$ns(id),"').prop('disabled',true)",sep="")))
+disableInput <- function(id, session) session$sendCustomMessage(type="jsCode", list(code=paste("$('#", session$ns(id), "').prop('disabled',true)", sep="")))
 #' @export
 #' @rdname disableInput
-enableInput <- function(id,session) session$sendCustomMessage(type="jsCode",list(code= paste("$('#",session$ns(id),"').prop('disabled',false)",sep="")))
+enableInput <- function(id, session) session$sendCustomMessage(type="jsCode", list(code=paste("$('#", session$ns(id), "').prop('disabled',false)", sep="")))
 
 #' Disable or enable a select input
 #' @export
 #' @rdname disableInput
-disableSelect <- function(id,session) session$sendCustomMessage(type="jsCode",list(code= paste("$('#",session$ns(id),"').selectize()[0].selectize.disable()",sep="")))
+disableSelect <- function(id, session) session$sendCustomMessage(type="jsCode", list(code=paste("$('#", session$ns(id), "').selectize()[0].selectize.disable()", sep="")))
 #' @export
 #' @rdname disableInput
-enableSelect <- function(id,session) session$sendCustomMessage(type="jsCode",list(code= paste("$('#",session$ns(id),"').selectize()[0].selectize.enable()",sep="")))
+enableSelect <- function(id, session) session$sendCustomMessage(type="jsCode", list(code=paste("$('#", session$ns(id), "').selectize()[0].selectize.enable()", sep="")))
 
 #' Force a reactive value to update (useful for \code{\link[shiny]{reactivePoll}} if the
 #' check function indicates the data hasn't changed, but the cached data needs updating anyway)
@@ -196,15 +218,15 @@ enableSelect <- function(id,session) session$sendCustomMessage(type="jsCode",lis
 #' @param r The reactive
 #' @export
 update_reactive <- function(r) {
-  attr(r,'observable')$.updateValue()
-  attr(r,'observable')$.dependents$invalidate()
+  attr(r, "observable")$.updateValue()
+  attr(r, "observable")$.dependents$invalidate()
 
-  #if the reactive is a reactive poll, we should also update the cookie to prevent a double update
-  ofe=environment(environment(r)$.origFunc)
-  if(exists('checkFunc',envir=ofe)) {
-    rv<-ofe$rv
-    valuesEnv<-get('impl',rv)$.values
-    value<-ofe$checkFunc()
-    assign('cookie',value,envir=valuesEnv)
+  # if the reactive is a reactive poll, we should also update the cookie to prevent a double update
+  ofe <- environment(environment(r)$.origFunc)
+  if (exists("checkFunc", envir=ofe)) {
+    rv <- ofe$rv
+    valuesEnv <- get("impl", rv)$.values
+    value <- ofe$checkFunc()
+    assign("cookie", value, envir=valuesEnv)
   }
 }
