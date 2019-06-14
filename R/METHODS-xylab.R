ae947f3745fe32e11c9bbe24c6a641a64d189923
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-14 19:54:53 +0100

    lint

--- R/METHODS-xylab.R
+++ R/METHODS-xylab.R
@@ -11,9 +11,9 @@ xlab.default <- function(x) {
 }
 #' @rdname xylabels.jms
 #' @export
-`xlab<-` <- function(x,value) UseMethod("xlab<-")
+`xlab<-` <- function(x, value) UseMethod("xlab<-")
 #' @export
-`xlab<-.default` <- function(x,value) {
+`xlab<-.default` <- function(x, value) {
   stop("Unable to assign x column for this class")
 }
 #' @rdname xylabels.jms
@@ -25,9 +25,9 @@ ylab.default <- function(x) {
 }
 #' @rdname xylabels.jms
 #' @export
-`ylab<-` <- function(x,value) UseMethod("ylab<-")
+`ylab<-` <- function(x, value) UseMethod("ylab<-")
 #' @export
-`ylab<-.default` <- function(x,value) {
+`ylab<-.default` <- function(x, value) {
   stop("Unable to assign y column for this class")
 }
 #' @rdname xylabels.jms
@@ -39,13 +39,13 @@ y2lab.default <- function(x) {
 }
 #' @rdname xylabels.jms
 #' @export
-`y2lab<-` <- function(x,value) UseMethod("y2lab<-")
+`y2lab<-` <- function(x, value) UseMethod("y2lab<-")
 #' @export
-`y2lab<-.default` <- function(x,value) {
+`y2lab<-.default` <- function(x, value) {
   stop("Unable to assign y2 column for this class")
 }
 
-#Internal methods for use in function definitions as default arguments
-xlab_<-function(...)xlab(...)
-ylab_<-function(...)ylab(...)
-y2lab_<-function(...)y2lab(...)
+# Internal methods for use in function definitions as default arguments
+xlab_ <- function(...) xlab(...)
+ylab_ <- function(...) ylab(...)
+y2lab_ <- function(...) y2lab(...)
