151a5bb80d2acd55ecd8d3baeac4a7058d63c8cc
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-14 19:08:31 +0100

    lint

--- R/DB-TABLE-save_load.R
+++ R/DB-TABLE-save_load.R
@@ -1,15 +1,15 @@
 #' @export
-save.jms.database.table <- function(x,...) {
-  if(inherits(parent.env(x),'jms.database')) {
-    log.debug('Preparing to save table')
-    parent.env(x)$.addTable(x$.name,x)
+save.jms.database.table <- function(x, ...) {
+  if (inherits(parent.env(x), "jms.database")) {
+    log.debug("Preparing to save table")
+    parent.env(x)$.addTable(x$.name, x)
   }
   return(x)
 }
 #' @export
-load.jms.database.table <- function(x,...) {
-  if(inherits(parent.env(x),'jms.database')) {
-    log.debug('Preparing to load table')
+load.jms.database.table <- function(x, ...) {
+  if (inherits(parent.env(x), "jms.database")) {
+    log.debug("Preparing to load table")
     parent.env(x)$.getTable(x$.name)
   } else {
     x
