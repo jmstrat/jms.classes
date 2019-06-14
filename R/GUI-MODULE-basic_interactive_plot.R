e80f5bccfd9097db0280a6ee0524c9ee97a86dcd
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-14 19:41:00 +0100

    lint

--- R/GUI-MODULE-basic_interactive_plot.R
+++ R/GUI-MODULE-basic_interactive_plot.R
@@ -6,43 +6,46 @@
 #' @param plotFunction Function to produce a plot
 #' @param ... Extra parameters are passed to \code{plotFunction}
 #' @examples
-#' ui <- basicInteractivePlotUI('myID')
+#' ui <- basicInteractivePlotUI("myID")
 #' server <- function(input, output, session) {
-#'     callModule(basicInteractivePlot, "myID",plotFunction=myFunction)
+#'   callModule(basicInteractivePlot, "myID", plotFunction=myFunction)
 #' }
 #' @rdname interactivePlot
 #' @export
 basicInteractivePlotUI <- function(id) {
   ns <- shiny::NS(id)
-  shiny::div(shinysky::shinyalert(ns("error"),auto.close.after = 5),
-             shiny::plotOutput(ns('plot'), height = 400,
-                               dblclick = ns('dblclick'),
-                               brush = shiny::brushOpts(
-                                 id = ns('brush'),
-                                 resetOnNew = TRUE
-                               )
-             ))
+  shiny::div(
+    shinysky::shinyalert(ns("error"), auto.close.after=5),
+    shiny::plotOutput(ns("plot"),
+      height=400,
+      dblclick=ns("dblclick"),
+      brush=shiny::brushOpts(
+        id=ns("brush"),
+        resetOnNew=TRUE
+      )
+    )
+  )
 }
 
 #' @rdname interactivePlot
 #' @export
-basicInteractivePlot <- function(input,output,session,plotFunction,...) {
-  hide_error('error',session)
-  plot_range <- shiny::reactiveValues(x = NULL, y = NULL)
+basicInteractivePlot <- function(input, output, session, plotFunction, ...) {
+  hide_error("error", session)
+  plot_range <- shiny::reactiveValues(x=NULL, y=NULL)
   output$plot <- shiny::renderPlot({
     tryCatch({
-      graphics::par(mar=c(3,4.1,0.5,2.1))
-      xlim=plot_range$x
-      ylim=plot_range$y
-      plotFunction(...,xlim=xlim,ylim=ylim)
-    }, error = function(e) {
-      show_error(e,'error',session)
+      graphics::par(mar=c(3, 4.1, 0.5, 2.1))
+      xlim <- plot_range$x
+      ylim <- plot_range$y
+      plotFunction(..., xlim=xlim, ylim=ylim)
+    }, error=function(e) {
+      show_error(e, "error", session)
     })
   })
   # zoom to the brush bounds; double click, reset the zoom.
-  shiny::observeEvent(input$dblclick,{
-    plot_range$x<-NULL
-    plot_range$y<-NULL
+  shiny::observeEvent(input$dblclick, {
+    plot_range$x <- NULL
+    plot_range$y <- NULL
   })
   shiny::observe({
     brush <- input$brush
