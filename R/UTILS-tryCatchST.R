0665754a0efbff493a788a64de0e644cb3a56afb
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-14 20:27:04 +0100

    lint

--- R/UTILS-tryCatchST.R
+++ R/UTILS-tryCatchST.R
@@ -3,23 +3,23 @@
 #' @param expr expression to execute
 #' @param error function of form error(e, stack_trace)
 #' @export
-tryCatchST = function(expr, error) {
+tryCatchST <- function(expr, error) {
   errorResult <- NULL
-  errorTracer = function(obj) {
+  errorTracer <- function(obj) {
     # Storing the call stack
-    calls = sys.calls()
+    calls <- sys.calls()
     obj$call <- sys.call(-4)
     errorResult <<- error(obj, calls)
   }
   errorIgnorer <- function(obj) {}
   parentenv <- parent.frame()
   logErrors <- function(expr) {
-    .Internal(.addCondHands('error', list(errorIgnorer), parentenv, environment(), FALSE))
-    .Internal(.addCondHands('error', list(errorTracer), parentenv, NULL, TRUE))
+    .Internal(.addCondHands("error", list(errorIgnorer), parentenv, environment(), FALSE))
+    .Internal(.addCondHands("error", list(errorTracer), parentenv, NULL, TRUE))
     expr
   }
   value <- logErrors(return(expr))[[1L]]
-  if(is.null(value)) errorResult else value
+  if (is.null(value)) errorResult else value
   # This is an attempt to simplify:
   # tryCatch(
   #   withCallingHandlers(expr,  error=errorTracer, caughtError=errorTracer),
@@ -31,6 +31,6 @@ tryCatchST = function(expr, error) {
 #' @rdname tryCatchST
 formatST <- function(st) {
   st <- rev(st)
-  st <- sapply(strsplit(as.character(st), '\n'), function(x) x[[1]])
-  paste0('#', 1:length(st), ' ', st, sep=' ', collapse='\n')
+  st <- sapply(strsplit(as.character(st), "\n"), function(x) x[[1]])
+  paste0("#", 1:length(st), " ", st, sep=" ", collapse="\n")
 }
