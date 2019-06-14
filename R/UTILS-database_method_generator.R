d66a3f814f3ccbef301236e8d21935a35910a7b2
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-14 20:16:50 +0100

    lint

--- R/UTILS-database_method_generator.R
+++ R/UTILS-database_method_generator.R
@@ -1,28 +1,32 @@
-database.method.generator <- function(self,overwrite=FALSE) {
-  function(name,func) {
-    if(exists(name,envir=self)&&!overwrite) return()
-    f<-formals(func)
-    nf<-names(f)
-    if('database'%in%nf) {
-      f[['database']]<-NULL
-      f[['database']]<-self
-    } else if('self' %in% nf) {
-      f[['self']]<-NULL
-      f[['self']]<-self
-    } else if('x' %in% nf) {
-      f[['x']]<-NULL
-      f[['x']]<-self
-    }  else {
-      stop('function is not a database method')
+database.method.generator <- function(self, overwrite=FALSE) {
+  function(name, func) {
+    if (exists(name, envir=self) && !overwrite) {
+      return()
     }
-    formals(func)<-f
-    self[[name]]=func
+    f <- formals(func)
+    nf <- names(f)
+    if ("database" %in% nf) {
+      f[["database"]] <- NULL
+      f[["database"]] <- self
+    } else if ("self" %in% nf) {
+      f[["self"]] <- NULL
+      f[["self"]] <- self
+    } else if ("x" %in% nf) {
+      f[["x"]] <- NULL
+      f[["x"]] <- self
+    } else {
+      stop("function is not a database method")
+    }
+    formals(func) <- f
+    self[[name]] <- func
   }
 }
 
-database.var.generator <- function(self,overwrite=FALSE) {
-  function(name,value) {
-    if(exists(name,envir=self)&&!overwrite) return()
-    self[[name]]=value
+database.var.generator <- function(self, overwrite=FALSE) {
+  function(name, value) {
+    if (exists(name, envir=self) && !overwrite) {
+      return()
+    }
+    self[[name]] <- value
   }
 }
