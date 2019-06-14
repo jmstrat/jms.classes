2a30c41b9d95c083d3deeb4df45cd984bd6c24c4
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-14 19:30:59 +0100

    lint

--- R/DO-xycol.R
+++ R/DO-xycol.R
@@ -1,54 +1,54 @@
 #' @export
 xcol.jms.data.object <- function(x) {
-  xcol=attr(x,'x_column')
-  if(is.null(xcol)) {
-    if(ncol(x)<=1) stop('Cannot get x column')
-    warning('Data type unknown, assuming 1st column for x axis')
-    xcol=1
+  xcol <- attr(x, "x_column")
+  if (is.null(xcol)) {
+    if (ncol(x) <= 1) stop("Cannot get x column")
+    warning("Data type unknown, assuming 1st column for x axis")
+    xcol <- 1
   }
   xcol
 }
 #' @export
-`xcol<-.jms.data.object` <- function(x,value) {
-  if(!is.numeric(value)) {
-    value = match(value ,colnames(x))
+`xcol<-.jms.data.object` <- function(x, value) {
+  if (!is.numeric(value)) {
+    value <- match(value, colnames(x))
   }
-  attr(x,'x_column')<-value
+  attr(x, "x_column") <- value
   x
 }
 #' @export
 ycol.jms.data.object <- function(x) {
-  ycol=attr(x,'y_column')
-  if(is.null(ycol)) {
-    if(ncol(x)<=1) stop('Cannot get y column')
-    warning('Data type unknown, assuming 2nd column for y axis')
-    ycol=2
+  ycol <- attr(x, "y_column")
+  if (is.null(ycol)) {
+    if (ncol(x) <= 1) stop("Cannot get y column")
+    warning("Data type unknown, assuming 2nd column for y axis")
+    ycol <- 2
   }
   ycol
 }
 #' @export
-`ycol<-.jms.data.object` <- function(x,value) {
-  if(!is.numeric(value)) {
-    value = match(value ,colnames(x))
+`ycol<-.jms.data.object` <- function(x, value) {
+  if (!is.numeric(value)) {
+    value <- match(value, colnames(x))
   }
-  attr(x,'y_column')<-value
+  attr(x, "y_column") <- value
   x
 }
 #' @export
 y2col.jms.data.object <- function(x) {
-  ycol=attr(x,'y2_column')
-  if(is.null(ycol)) {
-    if(ncol(x)<=1) stop('Cannot get y2 column')
-    warning('Data type unknown, assuming no y2 axis')
-    ycol = NA_integer_
+  ycol <- attr(x, "y2_column")
+  if (is.null(ycol)) {
+    if (ncol(x) <= 1) stop("Cannot get y2 column")
+    warning("Data type unknown, assuming no y2 axis")
+    ycol <- NA_integer_
   }
   ycol
 }
 #' @export
-`y2col<-.jms.data.object` <- function(x,value) {
-  if(!is.numeric(value)) {
-    value = match(value, colnames(x))
+`y2col<-.jms.data.object` <- function(x, value) {
+  if (!is.numeric(value)) {
+    value <- match(value, colnames(x))
   }
-  attr(x,'y2_column')<-value
+  attr(x, "y2_column") <- value
   x
 }
