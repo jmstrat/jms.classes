574d185fee2f61830b11da0a4dfce30e534461b3
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-14 19:53:44 +0100

    lint

--- R/METHODS-lock.R
+++ R/METHODS-lock.R
@@ -3,12 +3,12 @@
 #' @param x The database to lock
 #' @export
 #' @rdname jms.database.lock
-lock <- function(x) UseMethod('lock')
+lock <- function(x) UseMethod("lock")
 #' @export
 lock.default <- function(x) {
   stop("Unable to lock this object")
 }
-unlock <- function(x) UseMethod('unlock')
+unlock <- function(x) UseMethod("unlock")
 #' @export
 unlock.default <- function(x) {
   stop("Unable to unlock this object")
