4d5d298880bc47535253a33dcdb7fec173790913
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-14 19:02:33 +0100

    lint

--- R/DB-TABLE-equal.R
+++ R/DB-TABLE-equal.R
@@ -1,12 +1,12 @@
 #' @export
-all.equal.jms.database.table <- function(target,current,...) {
+all.equal.jms.database.table <- function(target, current, ...) {
   load(target)
   load(current)
   NextMethod()
 }
 
 #' @export
-`==.jms.database.table` <- function(e1,e2) {
+`==.jms.database.table` <- function(e1, e2) {
   load(e1)
   load(e2)
   NextMethod()
