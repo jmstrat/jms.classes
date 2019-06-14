044b308af4f11e92f0ad0d9f2c05125f7f6608a8
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-14 19:53:09 +0100

    lint

--- R/METHODS-export.R
+++ R/METHODS-export.R
@@ -4,20 +4,20 @@
 #' @param path The path to a csv file in which to save the data
 #' @rdname export
 #' @export
-export <- function(x,path) UseMethod("export")
+export <- function(x, path) UseMethod("export")
 #' @export
-export.default <- function(x,path) {
+export.default <- function(x, path) {
   stop("Unable to export data for this class")
 }
 #' @export
 export.list <- function(x, path) {
   # Do we have an export method for every item in the list?
-  if(any(as.character(.S3methods(export)) %in% paste0('export.',Reduce(intersect, lapply(x, class))))) {
+  if (any(as.character(.S3methods(export)) %in% paste0("export.", Reduce(intersect, lapply(x, class))))) {
     n <- names(x)
     fn <- tools::file_path_sans_ext(path)
     ext <- tools::file_ext(path)
 
-    paths <- paste0(fn, '_', n, '.', ext)
+    paths <- paste0(fn, "_", n, ".", ext)
 
     mapply(function(y, p) export(y, p), x, paths)
   } else {
@@ -25,5 +25,3 @@ export.list <- function(x, path) {
   }
   return(invisible())
 }
-
-
