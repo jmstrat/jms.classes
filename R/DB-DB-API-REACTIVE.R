1399ba584deb9598a3049fbcf720d19878adcb52
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-14 18:53:59 +0100

    lint

--- R/DB-DB-API-REACTIVE.R
+++ R/DB-DB-API-REACTIVE.R
@@ -7,8 +7,8 @@
 #' @param reactiveUpdateFreq Approximate number of milliseconds to wait between polling for updates.
 #' @return Database object
 #' @export
-jms.reactive.database <- function(path=NULL,reactiveSession=NULL,reactiveUpdateFreq=4000) {
-  as.jms.reactive.database(jms.database(path=path),reactiveSession,reactiveUpdateFreq)
+jms.reactive.database <- function(path=NULL, reactiveSession=NULL, reactiveUpdateFreq=4000) {
+  as.jms.reactive.database(jms.database(path=path), reactiveSession, reactiveUpdateFreq)
 }
 
 #' Check if an object is a jms.reactive.database
@@ -17,7 +17,7 @@ jms.reactive.database <- function(path=NULL,reactiveSession=NULL,reactiveUpdateF
 #' @return TRUE / FALSE
 #' @export
 is.jms.reactive.database <- function(x) {
-  return(inherits(x,"jms.reactive.database"))
+  return(inherits(x, "jms.reactive.database"))
 }
 
 #' Convert an object into a jms.database
@@ -25,73 +25,81 @@ is.jms.reactive.database <- function(x) {
 #' @param x The object to be converted
 #' @return The converted object
 #' @export
-as.jms.reactive.database <- function(x,reactiveSession=NULL,reactiveUpdateFreq=4000) UseMethod("as.jms.reactive.database")
+as.jms.reactive.database <- function(x, reactiveSession=NULL, reactiveUpdateFreq=4000) UseMethod("as.jms.reactive.database")
 
 #' @export
-as.jms.reactive.database.default <- function(x,reactiveSession=NULL,reactiveUpdateFreq=4000) {
-  tryCatch(as.jms.reactive.database(as.jms.database(x),reactiveSession=reactiveSession,reactiveUpdateFreq=reactiveUpdateFreq),error = function(e) stop("Unable to convert this class"))
+as.jms.reactive.database.default <- function(x, reactiveSession=NULL, reactiveUpdateFreq=4000) {
+  tryCatch(
+    as.jms.reactive.database(
+      as.jms.database(x),
+      reactiveSession=reactiveSession, reactiveUpdateFreq=reactiveUpdateFreq
+    ),
+    error=function(e) stop("Unable to convert this class")
+  )
 }
 
 #' @method as.jms.reactive.database jms.database
 #' @export
-as.jms.reactive.database.jms.database <- function(x,reactiveSession=NULL,reactiveUpdateFreq=4000) {
-  if(!inherits(reactiveSession,'ShinySession')) stop('reactiveSession must be a ShinySession object')
-  self<-get0('.self',envir=x, ifnotfound=x)
-  database.method <- database.method.generator(self,overwrite=TRUE)
-  database.var <- database.var.generator(self,overwrite=TRUE)
+as.jms.reactive.database.jms.database <- function(x, reactiveSession=NULL, reactiveUpdateFreq=4000) {
+  if (!inherits(reactiveSession, "ShinySession")) stop("reactiveSession must be a ShinySession object")
+  self <- get0(".self", envir=x, ifnotfound=x)
+  database.method <- database.method.generator(self, overwrite=TRUE)
+  database.var <- database.var.generator(self, overwrite=TRUE)
 
-  #Every time the hash updates, the reactive should invalidate
-  rv<-shiny::reactiveValues()
-  th<-self$.tableHashes
-  for(n in names(th)) rv[[n]]<-th[[n]]
-  database.var('.rv',rv)
-  database.var('.reactiveSession',reactiveSession)
+  # Every time the hash updates, the reactive should invalidate
+  rv <- shiny::reactiveValues()
+  th <- self$.tableHashes
+  for (n in names(th)) rv[[n]] <- th[[n]]
+  database.var(".rv", rv)
+  database.var(".reactiveSession", reactiveSession)
 
   force(reactiveUpdateFreq)
   if (is.function(reactiveUpdateFreq)) {
-    database.var('.reactiveUpdateFreq',reactiveUpdateFreq)
+    database.var(".reactiveUpdateFreq", reactiveUpdateFreq)
   } else {
-    database.var('.reactiveUpdateFreq',function() reactiveUpdateFreq)
+    database.var(".reactiveUpdateFreq", function() reactiveUpdateFreq)
   }
 
   updater <- function(name) {
     rv <- self$.rv
-    if(is.null(self$.path)) {
-      current=shiny::isolate(rv[[name]])
-      if(!is.numeric(current) || !length(current)) current=0
-      rv[[name]]<-current+1
+    if (is.null(self$.path)) {
+      current <- shiny::isolate(rv[[name]])
+      if (!is.numeric(current) || !length(current)) current <- 0
+      rv[[name]] <- current + 1
     } else {
-      rv[[name]]<-self$.tableHashes[[name]]
+      rv[[name]] <- self$.tableHashes[[name]]
     }
   }
 
-  st<-self$.saveTable
+  st <- self$.saveTable
   saveTable <- function(name, database=x) {
-    ret<-st(name=name,database=database)
+    ret <- st(name=name, database=database)
     updater(name)
     ret
   }
-  database.method('.saveTable',saveTable)
+  database.method(".saveTable", saveTable)
 
-  attr(x, "class") <- c("jms.reactive.database",class(x))
+  attr(x, "class") <- c("jms.reactive.database", class(x))
   return(x)
 }
 
 #' @export
-`[[.jms.reactive.database` <- function(x,name) {
-  if(startsWith(name,'.')) return(get(name,envir=x))
+`[[.jms.reactive.database` <- function(x, name) {
+  if (startsWith(name, ".")) {
+    return(get(name, envir=x))
+  }
   x$.loadDatabase()
-  if(!name %in% x$.self$.table_names) stop('Database table "',name,'" was not found')
+  if (!name %in% x$.self$.table_names) stop('Database table "', name, '" was not found')
   rs <- x$.reactiveSession
   ruf <- x$.reactiveUpdateFreq
   rv <- x$.rv
-  #Start polling to check for changes
+  # Start polling to check for changes
   shiny::observe({
-    if(!is.null(x$.path)) rv[[name]]<-x$.getHashForTable(name)
+    if (!is.null(x$.path)) rv[[name]] <- x$.getHashForTable(name)
     shiny::invalidateLater(ruf(), rs)
   })
   shiny::reactive({
-    rv[[name]] #Take reactive dependency (i.e. re-evaluate whenever the hash changes)
-    as.data.frame(`[[.jms.database`(x,name)) #NextMethod
-  }, label = NULL)
+    rv[[name]] # Take reactive dependency (i.e. re-evaluate whenever the hash changes)
+    as.data.frame(`[[.jms.database`(x, name)) # NextMethod
+  }, label=NULL)
 }
