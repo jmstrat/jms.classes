7cd5267773f854490b27cbb08dd5bee8f128c79b
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-14 19:54:02 +0100

    lint

--- R/METHODS-save_load.R
+++ R/METHODS-save_load.R
@@ -5,7 +5,7 @@ save <- function(x, ...) UseMethod("save")
 ## and set it to be the default method
 #' @export
 save.default <- base::save
-formals(save.default) <- c(formals(save.default), alist(... = ))
+formals(save.default) <- c(formals(save.default), alist(...=))
 
 ## make an S3 generic for load
 #' @export
@@ -14,4 +14,4 @@ load <- function(x, ...) UseMethod("load")
 ## and set it to be the default method
 #' @export
 load.default <- base::load
-formals(load.default) <- c(formals(load.default), alist(... = ))
+formals(load.default) <- c(formals(load.default), alist(...=))
