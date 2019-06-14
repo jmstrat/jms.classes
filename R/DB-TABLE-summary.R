c06a21973f8f07ad744b49d25e698d3cc4ec81cb
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-14 19:09:29 +0100

    lint

--- R/DB-TABLE-summary.R
+++ R/DB-TABLE-summary.R
@@ -1,30 +1,34 @@
 
 #' @export
-summary.jms.database.table <- function (object, ...)
-{
-  summ=data.frame(character(),numeric(),numeric(),numeric(),numeric(),numeric(),numeric(),numeric(),stringsAsFactors=FALSE,row.names = NULL)
-  names(summ)<-c('',"Min.", "1st Qu.", "Median", "Mean", "3rd Qu.", "Max.","NA's")
-  table=object$.table
-  nn=colnames(table)
-  a=0
-  for(i in 1:ncol(table)) {
-    if(nn[[i]]=='id') next()
-    a=a+1
-    summ[a,1]<-nn[[i]]
-    o<-table[,i]
-    if(!is.numeric(o)) next()
+summary.jms.database.table <- function(object, ...) {
+  summ <- data.frame(
+    character(), numeric(), numeric(), numeric(),
+    numeric(), numeric(), numeric(), numeric(),
+    stringsAsFactors=FALSE, row.names=NULL
+  )
+  names(summ) <- c("", "Min.", "1st Qu.", "Median", "Mean", "3rd Qu.", "Max.", "NA's")
+  table <- object$.table
+  nn <- colnames(table)
+  a <- 0
+  for (i in 1:ncol(table)) {
+    if (nn[[i]] == "id") next()
+    a <- a + 1
+    summ[a, 1] <- nn[[i]]
+    o <- table[, i]
+    if (!is.numeric(o)) next()
     nas <- is.na(o)
     o <- o[!nas]
     qq <- stats::quantile(o)
     qq <- signif(c(qq[1L:3L], mean(o), qq[4L:5L]), 3)
-    if (any(nas))
-      qq<-c(qq, `NA's` = sum(nas))
-    else
-      qq<-c(qq,NA)
-    summ[a,2:8]<-qq
+    if (any(nas)) {
+      qq <- c(qq, `NA's`=sum(nas))
+    } else {
+      qq <- c(qq, NA)
+    }
+    summ[a, 2:8] <- qq
   }
-  summ=as.matrix(summ)
-  dimnames(summ)[[1L]]<-rep.int("", nrow(summ))
-  summ[is.na(summ)]<-''
-  print(summ,quote=FALSE)
+  summ <- as.matrix(summ)
+  dimnames(summ)[[1L]] <- rep.int("", nrow(summ))
+  summ[is.na(summ)] <- ""
+  print(summ, quote=FALSE)
 }
