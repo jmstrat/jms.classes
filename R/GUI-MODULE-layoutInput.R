522f1f1547f3b8410701cad7d1ca591ce2f5ba6b
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-14 19:48:35 +0100

    lint

--- R/GUI-MODULE-layoutInput.R
+++ R/GUI-MODULE-layoutInput.R
@@ -8,37 +8,38 @@
 #' @rdname layout_module
 #' @export
 layoutInput <- function(id, default_layout, plot_names, can_add_plots, min_plots) {
+  shiny::addResourcePath(prefix="www", directoryPath=system.file("www", package="jms.classes"))
 
-  shiny::addResourcePath(prefix = 'www', directoryPath = system.file('www', package='jms.classes'))
-
-  buttons = shiny::tagList()
-  if(can_add_plots) {
-    buttons = shiny::tags$div(
+  buttons <- shiny::tagList()
+  if (can_add_plots) {
+    buttons <- shiny::tags$div(
       class="controls",
-      shiny::tags$a(id="add-button", href='#', 'Add Plot'),
-      shiny::tags$span('|'),
-      shiny::tags$a(id="remove-button", href='#', 'Remove Plot')
+      shiny::tags$a(id="add-button", href="#", "Add Plot"),
+      shiny::tags$span("|"),
+      shiny::tags$a(id="remove-button", href="#", "Remove Plot")
     )
   }
 
   shiny::tagList(
     htmltools::singleton(
       shiny::tags$head(
-        shiny::tags$link(rel = "stylesheet", type = "text/css", href = "www/gridstack.min.css"),
-        shiny::tags$link(rel = "stylesheet", type = "text/css", href = "www/layoutInput.css"),
-        shiny::tags$script(src = "www/jquery-ui.min.js"),
-        shiny::tags$script(src = "www/lodash.min.js"),
-        shiny::tags$script(src = "www/gridstack.min.js"),
-        shiny::tags$script(src = "www/gridstack.jQueryUI.min.js"),
-        shiny::tags$script(src = "www/layoutInput.js")
+        shiny::tags$link(rel="stylesheet", type="text/css", href="www/gridstack.min.css"),
+        shiny::tags$link(rel="stylesheet", type="text/css", href="www/layoutInput.css"),
+        shiny::tags$script(src="www/jquery-ui.min.js"),
+        shiny::tags$script(src="www/lodash.min.js"),
+        shiny::tags$script(src="www/gridstack.min.js"),
+        shiny::tags$script(src="www/gridstack.jQueryUI.min.js"),
+        shiny::tags$script(src="www/layoutInput.js")
       )
     ),
-    shiny::tags$div(id=id, class="layout-input",
+    shiny::tags$div(
+      id=id, class="layout-input",
       buttons,
-      shiny::tags$div(class="grid-stack",
-                      "data-min-plots" = min_plots,
-                      "data-plot-names" = shiny:::toJSON(plot_names),
-                      "data-default-layout" = shiny:::toJSON(default_layout)
+      shiny::tags$div(
+        class="grid-stack",
+        "data-min-plots"=min_plots,
+        "data-plot-names"=shiny:::toJSON(plot_names),
+        "data-default-layout"=shiny:::toJSON(default_layout)
       )
     )
   )
