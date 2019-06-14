95816cfd4fb2bdaa4a5d7d12b3f1df2f26db7585
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-14 19:05:14 +0100

    lint

--- R/DB-TABLE-ID-get.R
+++ R/DB-TABLE-ID-get.R
@@ -1,17 +1,17 @@
 #' Get an element from the table
 #'
 #' @export
-`[.jms.database.table.id` <- function(x,i=T,j=T) {
-  if(!length(i)) i<-TRUE
-  if(!length(j)) j<-TRUE
-  ids=x$.table[,'id']
-  inum=is.numeric(i)
-  iltz=i<0
-  if(any(inum)) {
-    iids<-which(ids%in%abs(i[inum]))
-    if(!length(iids)) stop ('ID not found')
-    i[inum]<-iids
+`[.jms.database.table.id` <- function(x, i=T, j=T) {
+  if (!length(i)) i <- TRUE
+  if (!length(j)) j <- TRUE
+  ids <- x$.table[, "id"]
+  inum <- is.numeric(i)
+  iltz <- i < 0
+  if (any(inum)) {
+    iids <- which(ids %in% abs(i[inum]))
+    if (!length(iids)) stop("ID not found")
+    i[inum] <- iids
   }
-  if(any(iltz))  i[iltz] <- i[iltz]*-1
-  `[.jms.database.table`(x,i,j)
+  if (any(iltz)) i[iltz] <- i[iltz] * -1
+  `[.jms.database.table`(x, i, j)
 }
