42a556075db5c07077acdf6e655c6cea2abd73cb
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-14 19:54:28 +0100

    lint

--- R/METHODS-View.R
+++ R/METHODS-View.R
@@ -1,8 +1,8 @@
 ## make an S3 generic for View
 #' @export
-View <- function(...) UseMethod('View')
+View <- function(...) UseMethod("View")
 ## take the usual definition of View,
 ## and set it to be the default method
 ## this mget must be at run time to capture the RStudio override (i.e. wrapped in function)
 #' @export
-View.default <- function(...) mget('View', as.environment('package:utils'))[[1]](...)
+View.default <- function(...) mget("View", as.environment("package:utils"))[[1]](...)
