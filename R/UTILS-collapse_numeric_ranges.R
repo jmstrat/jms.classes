e6afd5fd1a3923e242c7772ea2876177715a171f
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-14 20:16:03 +0100

    lint

--- R/UTILS-collapse_numeric_ranges.R
+++ R/UTILS-collapse_numeric_ranges.R
@@ -4,16 +4,19 @@
 #' @param min_collapse Minimum length of range before collapsing
 #' @export
 #' @examples
-#' s <- c(1,2,3,4,8,9,14,15,16,19,21,24,25,26)
+#' s <- c(1, 2, 3, 4, 8, 9, 14, 15, 16, 19, 21, 24, 25, 26)
 #'
 #' paste0(numeric_to_string_ranges(s), collapse=", ")
-#' paste0(numeric_to_string_ranges(s,3), collapse=", ")
-#' paste0(numeric_to_string_ranges(s,4), collapse=", ")
-numeric_to_string_ranges <- function(x, min_collapse=2){
+#' paste0(numeric_to_string_ranges(s, 3), collapse=", ")
+#' paste0(numeric_to_string_ranges(s, 4), collapse=", ")
+numeric_to_string_ranges <- function(x, min_collapse=2) {
   diffs <- c(1, diff(x))
   is_step <- diffs != 1
   range_list <- split(x, cumsum(is_step))
-  unlist(lapply(range_list,
-                function(r) if(length(r) < min_collapse) as.character(r) else paste0(r[1], "-", r[length(r)])),
-         use.names = FALSE)
+  unlist(lapply(
+    range_list,
+    function(r) if (length(r) < min_collapse) as.character(r) else paste0(r[1], "-", r[length(r)])
+  ),
+  use.names=FALSE
+  )
 }
