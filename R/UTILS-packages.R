2e6f2936041b06bba90b7cab1e3dbeeedb9f2143
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-14 20:25:47 +0100

    lint

--- R/UTILS-packages.R
+++ R/UTILS-packages.R
@@ -2,18 +2,18 @@
 #' @param ... Packages to test
 #' @param purpose Used in error message
 #' @export
-assert_packages <- function(..., purpose='This function') {
-  package_list = c(...)
-  missing_packages = c()
-  for(p in package_list) {
-    if(!requireNamespace(p, quietly=TRUE)) {
-      missing_packages = c(missing_packages, p)
+assert_packages <- function(..., purpose="This function") {
+  package_list <- c(...)
+  missing_packages <- c()
+  for (p in package_list) {
+    if (!requireNamespace(p, quietly=TRUE)) {
+      missing_packages <- c(missing_packages, p)
     }
   }
-  if(length(missing_packages) > 0) {
+  if (length(missing_packages) > 0) {
+    command <- sprintf("install.packages(c('%s'))", paste0(missing_packages, collapse="', '"))
 
-    command = sprintf("install.packages(c('%s'))", paste0(missing_packages, collapse="', '"))
-
-    stop(purpose, ' requires additional packages. Run the following command to install them, then try again:\n', command, '\n\nYou may need to restart R after running this command to complete the installation.', call.=FALSE)
+    stop(purpose, " requires additional packages. Run the following command to install them, then try again:\n",
+         command, "\n\nYou may need to restart R after running this command to complete the installation.", call.=FALSE)
   }
 }
