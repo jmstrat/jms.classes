9849ee034283f5c8ed8ec791ba7af05680c7bdcf
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-14 20:12:36 +0100

    lint

--- R/PACKAGE-onload.R
+++ R/PACKAGE-onload.R
@@ -1,25 +1,25 @@
-config_dir=NULL
-config_db<-NULL
-persistent_settings<-NULL
+config_dir <- NULL
+config_db <- NULL
+persistent_settings <- NULL
 
 .onLoad <- function(libname, pkgname) {
-  config_dir<<-rappdirs::user_config_dir('jms.packages', 'jms')
-  dir.create(config_dir, showWarnings = FALSE, recursive=TRUE)
-  #Load config db
-  db_dir <- file.path(config_dir, 'config-database')
-  dir.create(db_dir, showWarnings = FALSE,recursive=TRUE)
+  config_dir <<- rappdirs::user_config_dir("jms.packages", "jms")
+  dir.create(config_dir, showWarnings=FALSE, recursive=TRUE)
+  # Load config db
+  db_dir <- file.path(config_dir, "config-database")
+  dir.create(db_dir, showWarnings=FALSE, recursive=TRUE)
   config_db <<- jms.database(db_dir)
   persistent_settings <<- tryCatch({
-    config_db[['persistent_settings']]
-  }, error = function(e) {
-    config_db[['persistent_settings']] <- jms.database.table(key=character(),value=character())
-    config_db[['persistent_settings']]
+    config_db[["persistent_settings"]]
+  }, error=function(e) {
+    config_db[["persistent_settings"]] <- jms.database.table(key=character(), value=character())
+    config_db[["persistent_settings"]]
   })
-  #Load the project db
+  # Load the project db
   project_database()
 
-  #Register input handlers with shiny
-  if(requireNamespace("shiny", quietly=TRUE)) {
+  # Register input handlers with shiny
+  if (requireNamespace("shiny", quietly=TRUE)) {
     shiny::registerInputHandler("jms.matrix", shinyInputConvertToMatrix, force=TRUE)
   }
 
@@ -30,38 +30,38 @@ persistent_settings<-NULL
 get_persistent_setting <- function(key) {
   tryCatchST({
     get_key(key)
-  }, error = function(e, st) {
-    log.error('Error whilst getting persistent setting %s: %s\n%s', key, e, formatST(st))
-    warning('Could not get setting ', key, immediate.=TRUE)
+  }, error=function(e, st) {
+    log.error("Error whilst getting persistent setting %s: %s\n%s", key, e, formatST(st))
+    warning("Could not get setting ", key, immediate.=TRUE)
     NULL
   })
 }
 
 #' @export
-set_persistent_setting <- function(key,value) {
+set_persistent_setting <- function(key, value) {
   tryCatchST({
     set_key(key, value)
-  }, error = function(e, st) {
-    log.error('Error whilst setting persistent setting %s = %s: %s\n%s', key, value, e, formatST(st))
-    warning('Could not set ', key, immediate.=TRUE)
+  }, error=function(e, st) {
+    log.error("Error whilst setting persistent setting %s = %s: %s\n%s", key, value, e, formatST(st))
+    warning("Could not set ", key, immediate.=TRUE)
     NULL
   })
 }
 
 get_key <- function(key, table=persistent_settings) {
-  i=which(key == table[,'key'])
-  if(length(i)) {
-    table[i,'value']
+  i <- which(key == table[, "key"])
+  if (length(i)) {
+    table[i, "value"]
   } else {
     NULL
   }
 }
 
 set_key <- function(key, value, table=persistent_settings) {
-  i=which(key == table[,'key'])
-  if(length(i)) {
-    table[i,'value']<-value
+  i <- which(key == table[, "key"])
+  if (length(i)) {
+    table[i, "value"] <- value
   } else {
-    table[nrow(table)+1,]<-list(key,value)
+    table[nrow(table) + 1, ] <- list(key, value)
   }
 }
