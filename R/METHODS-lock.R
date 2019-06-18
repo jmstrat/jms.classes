931634d1970c74a285f5722dd1959f83b81fea87
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-18 21:38:49 +0100

    Missing export

--- R/METHODS-lock.R
+++ R/METHODS-lock.R
@@ -4,11 +4,16 @@
 #' @export
 #' @rdname jms.database.lock
 lock <- function(x) UseMethod("lock")
+
 #' @export
 lock.default <- function(x) {
   stop("Unable to lock this object")
 }
+
+#' @export
+#' @rdname jms.database.lock
 unlock <- function(x) UseMethod("unlock")
+
 #' @export
 unlock.default <- function(x) {
   stop("Unable to unlock this object")
